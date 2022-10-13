134d22cb909a44163db4bb99af8156e38a8218c0
klauscritchley <klaustorger@gmail.com>
2022-10-13 17:38:34 -0500

    reversed y axis on gamepad

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RegualarTeleOp/mechme.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RegualarTeleOp/mechme.java
@@ -58,7 +58,7 @@ public class mechme extends LinearOpMode {
                 armL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             }
 
-            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+            double y = gamepad1.left_stick_y; // Remember, this is reversed!
             double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
             double rx = gamepad1.right_stick_x;
 
