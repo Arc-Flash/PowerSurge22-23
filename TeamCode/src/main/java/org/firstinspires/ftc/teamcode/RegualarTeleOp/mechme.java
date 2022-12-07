5d88c06844f49c6f412d2de0d3e6af0a9726c77b
klauscritchley <klaustorger@gmail.com>
2022-12-06 20:36:45 -0600

    Update mechme.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RegualarTeleOp/mechme.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RegualarTeleOp/mechme.java
@@ -16,7 +16,7 @@ public class mechme extends LinearOpMode {
         DcMotor motorBackLeft = hardwareMap.dcMotor.get("backLeft");
         DcMotor motorFrontRight = hardwareMap.dcMotor.get("frontRight");
         DcMotor motorBackRight = hardwareMap.dcMotor.get("backRight");
-        //DcMotor arm = hardwareMap.dcMotor.get("andrew"); //Motor attatched to the back of the robot
+        DcMotor arm = hardwareMap.dcMotor.get("andrew"); //Motor attatched to the back of the robot
         DcMotor elbowMotor = hardwareMap.dcMotor.get("20centpotatoes"); // Motor that is locate on the arm
         Servo sl = hardwareMap.servo.get("s1");
         Servo sr = hardwareMap.servo.get("sr");
@@ -28,8 +28,8 @@ public class mechme extends LinearOpMode {
         motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
         motorFrontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         motorBackLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        //arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         elbowMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         elbowMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         sl.setDirection(Servo.Direction.FORWARD);
@@ -45,7 +45,7 @@ public class mechme extends LinearOpMode {
         // continuous loop that runs after after the play button is pushed
         if (isStopRequested()) return;
 
-        //arm.setTargetPosition(sholderPosition);
+        arm.setTargetPosition(sholderPosition);
         elbowMotor.setTargetPosition(elbowJoint);
 
         // Runs while the opmode is active
@@ -58,19 +58,18 @@ public class mechme extends LinearOpMode {
             }
 
             // Runs when the dpad is pushed down on game-pad 2
-            /*if (gamepad2.dpad_down) {
-                sholderPosition = sholderPosition - 1;
+            if (gamepad2.a) {
+                sholderPosition = -2;
 
                 arm.setTargetPosition(sholderPosition);
             }
 
             // Runs when the dpad is pushed up on game-pad 2
-            if (gamepad2.dpad_up) {
-                sholderPosition = sholderPosition + 1;
+            if (gamepad2.x) {
+                sholderPosition = -30;
 
                 arm.setTargetPosition(sholderPosition);
             }
-            */
 
 
             // Runs when the dpad is pushed left on game-pad 2
@@ -88,14 +87,15 @@ public class mechme extends LinearOpMode {
 
             elbowMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             elbowMotor.setPower(1);
-
+            arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            arm.setPower(1);
 
 
             //Runs when the left stick is pressed down
             //Moves servos position so that they are closed
             if (gamepad2.left_bumper) {
-                sl.setPosition(.7);
-                sr.setPosition(.7);
+                sl.setPosition(1);
+                sr.setPosition(1);
             }
 
             //Runs when the right stick is pressed down
@@ -128,17 +128,25 @@ public class mechme extends LinearOpMode {
 
             double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
             double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
-            double rightX = gamepad1.right_stick_x;
+            double rightX = -gamepad1.right_stick_x;
             final double v1 = -r * Math.cos(robotAngle) + rightX;
             final double v2 = -r * Math.sin(robotAngle) - rightX;
             final double v3 = -r * Math.sin(robotAngle) + rightX;
             final double v4 = -r * Math.cos(robotAngle) - rightX;
 
-            if (sens == false) {
-                motorFrontLeft.setPower(v1);
-                motorFrontRight.setPower(v2);
-                motorBackLeft.setPower(v3);
-                motorBackRight.setPower(v4);
+            if (gamepad1.right_bumper) {
+                sens = true;
+            }
+            else {
+                sens = false;
+            }
+
+
+            if (gamepad2.left_trigger == 1 && gamepad2.right_trigger == 1) {
+                motorFrontLeft.setPower(1);
+                motorBackLeft.setPower(1);
+                motorBackRight.setPower(1);
+                motorFrontRight.setPower(1);
             }
             else if (sens == true) {
                 motorFrontLeft.setPower(v1/3);
@@ -146,11 +154,21 @@ public class mechme extends LinearOpMode {
                 motorBackLeft.setPower(v3/3);
                 motorBackRight.setPower(v4/3);
             }
+            else {
+                motorFrontLeft.setPower(v1);
+                motorFrontRight.setPower(v2);
+                motorBackLeft.setPower(v3);
+                motorBackRight.setPower(v4);
+            }
 
-            
             telemetry.addData("20 target:", elbowMotor.getTargetPosition());
             telemetry.addData("20 current:", elbowMotor.getCurrentPosition());
             telemetry.addData("20 power:", elbowMotor.getPower());
+            telemetry.addData("Arm Power" , arm.getPower());
+            telemetry.addData("Arm Target", arm.getTargetPosition());
+            telemetry.addData("Arm Current", arm.getCurrentPosition());
+            telemetry.addData("Servo 1 position", sr.getPosition());
+            telemetry.addData("Servo Left Position", sl.getPosition());
             telemetry.update();
         }
     }
